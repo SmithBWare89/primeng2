e205196da1b21c919271ab16d0132e5b7d12aeb8
Stephon Smith <smithwrestling89@gmail.com>
2021-10-21 15:49:48 -0400

    First Commit
    Added PrimeNG and Prime NG flex. Working version.

---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavigationRoutingModule } from './navigation-routing.module';
+import { MenuComponent } from './menu/menu.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+@NgModule({
+  declarations: [
+    MenuComponent,
+    NavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    NavigationRoutingModule
+  ],
+  exports: [
+    NavigationComponent
+  ]
+})
+export class NavigationModule { }
